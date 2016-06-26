ed9e1166b50c6af334f068283e9473fab77699d2
Hasan Kahraman <hasankahraman89@hotmail.com>
2016-06-26 15:04:36 +0300

    Add files via upload

---
+++ winGaleri/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace winGaleri
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.mnuDosya = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuAc = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuKaydet = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.mnuEkle = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripSeparator();
+            this.mnuCikis = new System.Windows.Forms.ToolStripMenuItem();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuDosya});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(566, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // mnuDosya
+            // 
+            this.mnuDosya.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuAc,
+            this.mnuKaydet,
+            this.toolStripMenuItem1,
+            this.mnuEkle,
+            this.toolStripMenuItem2,
+            this.mnuCikis});
+            this.mnuDosya.Name = "mnuDosya";
+            this.mnuDosya.Size = new System.Drawing.Size(51, 20);
+            this.mnuDosya.Text = "Dosya";
+            // 
+            // mnuAc
+            // 
+            this.mnuAc.Name = "mnuAc";
+            this.mnuAc.Size = new System.Drawing.Size(152, 22);
+            this.mnuAc.Text = "Aç";
+            this.mnuAc.Click += new System.EventHandler(this.mnuAc_Click);
+            // 
+            // mnuKaydet
+            // 
+            this.mnuKaydet.Name = "mnuKaydet";
+            this.mnuKaydet.Size = new System.Drawing.Size(152, 22);
+            this.mnuKaydet.Text = "Kaydet";
+            this.mnuKaydet.Click += new System.EventHandler(this.mnuKaydet_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(149, 6);
+            // 
+            // mnuEkle
+            // 
+            this.mnuEkle.Name = "mnuEkle";
+            this.mnuEkle.Size = new System.Drawing.Size(152, 22);
+            this.mnuEkle.Text = "Ekle";
+            this.mnuEkle.Click += new System.EventHandler(this.mnuEkle_Click);
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(149, 6);
+            // 
+            // mnuCikis
+            // 
+            this.mnuCikis.Name = "mnuCikis";
+            this.mnuCikis.Size = new System.Drawing.Size(152, 22);
+            this.mnuCikis.Text = "Çıkış";
+            this.mnuCikis.Click += new System.EventHandler(this.mnuCikis_Click);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 24);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(566, 331);
+            this.flowLayoutPanel1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(566, 355);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem mnuDosya;
+        private System.Windows.Forms.ToolStripMenuItem mnuAc;
+        private System.Windows.Forms.ToolStripMenuItem mnuKaydet;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem mnuEkle;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem mnuCikis;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+    }
+}
+
---
+++ winGaleri/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace winGaleri
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void mnuEkle_Click(object sender, EventArgs e)
+        {
+            // Ekle seçeneği ile belirlediğimiz dosya türündeki dosyaları seçmek için open dialog açılır. Çoklu seçim yaptırılır(bkz. ofd.Multiselect = true)
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "JPEG Resimler(*.jpg;*.jpeg)|*.jpg;*.jpeg|PNG Resimler(*.png)|*.png|Tüm Dosyalar(*.jpg;*.jpeg;*.png;*.bmp)|*.jpg;*.jpeg;*.png;*.bmp";
+            ofd.FilterIndex = 3;
+            ofd.Title = "Resimleri Seç";
+            ofd.Multiselect = true;
+            ofd.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+
+            if(ofd.ShowDialog() == DialogResult.OK)
+            {
+                // Seçilen dosya konumları elde edilir.
+                string[] dosyaKonumlari = ofd.FileNames;
+
+                // Her dosya konumu için picturebox FlowPanel'e eklenir.
+                foreach (string dosyaKonum in dosyaKonumlari)
+                {
+                    PictureBoxEkle(dosyaKonum);
+                }
+            }
+
+
+        }
+
+        private void PictureBoxEkle(string dosyaKonum)
+        {
+            // Önce bir panel oluşturulur. Picturebox etrafında border çıkartmak için..
+            Panel pnl = new Panel();
+            pnl.BorderStyle = BorderStyle.FixedSingle;
+            pnl.Width = 75;
+            pnl.Height = 75;
+
+            // Picturebox oluşturulur ve metoda gelen parametre ile göstereceği dosyaKOnum ayarlanır.
+            PictureBox pb = new PictureBox();
+            pb.SizeMode = PictureBoxSizeMode.Zoom;
+            pb.ImageLocation = dosyaKonum;
+            pb.Dock = DockStyle.Fill;
+
+            // Picturebox'a tıklanınca çalışacak olan metot işaretlenir(Pb_Click).
+            pb.Click += Pb_Click;
+
+            // Picturebox panel'e eklenir.
+            pnl.Controls.Add(pb);
+
+            // Panel, FlowPanel'e eklenir.
+            flowLayoutPanel1.Controls.Add(pnl);
+        }
+
+
+        private void Pb_Click(object sender, EventArgs e)
+        {
+            // Picturebox'a tıklandığında..
+
+            // Ana formumuzdaki picturebox elde edilir.
+            PictureBox pbox = (PictureBox)sender;
+
+            // Yeni form oluşturulur.(Dilerseniz projeye form ekleyerek yapabilirsiniz)
+            Form frm = new Form();
+            frm.Text = "Resim Önizleme";
+            frm.Width = 250;
+            frm.Height = 250;
+
+            // Yeni picturebox oluşturulur. Eğer Ana form'daki picturebox'ı kullanırsak, yeni form'a, ana form'daki picturebox'ı taşır. Amaç yeni bir picturebox'ı yeni form'a eklemek ve ayarlarını ana form'daki picturebox ile aynı yapmak.
+            PictureBox pbNew = new PictureBox();
+            pbNew.ImageLocation = pbox.ImageLocation;
+            pbNew.SizeMode = pbox.SizeMode;
+            pbNew.Dock = DockStyle.Fill;
+
+            // Yeni picturebox yeni form'a eklenir.
+            frm.Controls.Add(pbNew);
+
+            // Form show edilerek gösterilir. Ana form'a geçebilir şekilde açılır.
+            frm.Show();
+        }
+
+        private void mnuKaydet_Click(object sender, EventArgs e)
+        {
+            // Dosya konumlarını toplayacağımız bir liste tanımlanır.
+            List<string> kaydedilecekDosyaKonumalari =
+                new List<string>();
+
+            // flowLayoutPanel1.Controls özelliği ile ana form'daki FlowPanel'e eklemiş olduğumuz panellere ulaşırız(Panel içinde picturebox). 
+            foreach (Panel pnl in flowLayoutPanel1.Controls)
+            {
+                // Biliyoruz ki her bir panel içinde sadece picturebox var.
+                // panel içindeki picturebox elde edilerek, bir değişkene bağlanır.
+                PictureBox pbox = (PictureBox)pnl.Controls[0];
+                // picturebox'ın gösterdiği dosya konumu listeye eklenir.
+                kaydedilecekDosyaKonumalari.Add(pbox.ImageLocation);
+            }
+
+            // Kaydetme dialoğu açılır..
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Title = "Galeri Kaydet";
+            sfd.Filter = "Galeri Dosyaları(*.glr)|*.glr";   // uzantıyı biz uydurduk.
+            sfd.FilterIndex = 1;
+            sfd.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+
+            if(sfd.ShowDialog() == DialogResult.OK)
+            {
+                // Dosya konumları seçilen konuma kaydedilir.
+                System.IO.File.WriteAllLines(sfd.FileName, kaydedilecekDosyaKonumalari);
+                MessageBox.Show("Galeri kaydedilmiştir.");
+            }
+        }
+
+        private void mnuAc_Click(object sender, EventArgs e)
+        {
+            // Bizim belirlediğimiz uzantıdaki dosyalar açılması için opendialog kullanılır.
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "Galeri Aç";
+            ofd.Filter = "Galeri Dosyaları(*.glr)|*.glr";
+            ofd.FilterIndex = 1;
+            ofd.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures); // resimlerim klasör'ü başlangıçta açılsın şekilde ayarlanır.
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                // Seçilen dosyadan resimlerin konumları okunur.(satır satır)
+                string[] dosyalar = System.IO.File.ReadAllLines(ofd.FileName);
+
+                // FlowPanel temizlenir.
+                flowLayoutPanel1.Controls.Clear();
+
+                // Her bir resim konumu kullanılarak resim flowPanel'e "Ekle" tuşundaki gibi eklenir.
+                foreach (string dosyaKonum in dosyalar)
+                {
+                    PictureBoxEkle(dosyaKonum);
+                }
+            }
+        }
+
+        private void mnuCikis_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
